ebc78350f725e6e421d6f17e1dd1fe5e2d8630f1
GANESH BASAVOJU <basavojuganesh@gmail.com>
2026-01-09 20:21:36 +0530

    deploy changes20

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const app = require('../src/app');
 const connectDB = require('../src/config/db');
 
-// Connect to Database
-connectDB();
-
-// Export the Express API
-module.exports = app;
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
