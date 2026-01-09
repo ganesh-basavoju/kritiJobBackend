2d5c08ad3f982538af65fa6c7a26cdc137ea6fce
GANESH BASAVOJU <basavojuganesh@gmail.com>
2026-01-09 20:09:48 +0530

    deploy changes23

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,39 @@
 const mongoose = require('mongoose');
 const logger = require('./logger');
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    logger.info(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    logger.error(`Error: ${error.message}`);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false, // Disable Mongoose buffering to fail fast if not connected
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+      logger.info(`MongoDB Connected: ${mongoose.connection.host}`);
+      return mongoose;
+    }).catch(err => {
+        logger.error(`Error: ${err.message}`);
+        throw err;
+    });
+  }
+  
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 };
 
 module.exports = connectDB;
