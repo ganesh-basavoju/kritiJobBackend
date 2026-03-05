0640bde6b328a386908046842e583e449e9851d5
Rohit Mondal <142654843+Rohit-Mondal-46@users.noreply.github.com>
2026-03-05 22:38:37 +0530

    Add authorization to job feed route

--- src/modules/jobs/jobs.routes.js
+++ src/modules/jobs/jobs.routes.js
@@ -6,7 +6,7 @@ const { authorize } = require('../../middlewares/rbac.middleware');
 const router = express.Router();
 
 router.get('/', getJobs);
-router.get('/feed', getJobFeed);
+router.get('/feed', authorize('candidate'), getJobFeed);
 router.get('/my-jobs', protect, authorize('employer'), getMyJobs);
 router.get('/:id', getJob);
 
